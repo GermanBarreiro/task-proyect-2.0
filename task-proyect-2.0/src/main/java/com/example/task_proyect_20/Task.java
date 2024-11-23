6ca804146324d5103e0c5397d59a141dc8c56e8f
german.bf <german.bf@avangenio.com>
2024-11-23 18:48:37 -0500

    first commit

---
+++ task-proyect-2.0/src/main/java/com/example/task_proyect_20/Task.java
@@ -0,0 +1,57 @@
+package com.example.task_proyect_20;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+
+    public Task() {
+    }
+    public Task(boolean completed, String description, Long id, String title) {
+        this.completed = completed;
+        this.description = description;
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
---
+++ task-proyect-2.0/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO task (title, description, completed) VALUES ('Tarea 1', 'Descripción de la tarea 1', false);
+INSERT INTO task (title, description, completed) VALUES ('Tarea 2', 'Descripción de la tarea 2', true);
+INSERT INTO task (title, description, completed) VALUES ('Tarea 3', 'Descripción de la tarea 3', false);
